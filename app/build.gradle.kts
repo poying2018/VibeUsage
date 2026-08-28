aa8060b2ad452b6e655b263b936014f11d31a617
poying2018 <poying2018@users.noreply.github.com>
2026-08-28 21:17:57 +0800

    release: v2.9.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         // 低版本会自动降级为无折射/无模糊的半透明玻璃，不会崩溃。
         minSdk = 26
         targetSdk = 34
-        versionCode = 36
-        versionName = "2.9.9"
+        versionCode = 37
+        versionName = "2.9.10"
         vectorDrawables { useSupportLibrary = true }
     }
 
